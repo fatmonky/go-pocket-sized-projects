61a33463f5f86a7b78f6ebc557392f16d1c89d82
PJ Teh <peijing.teh.dev@gmail.com>
2025-06-09 11:10:16 +0800

    listing 2.3.2: add table-driven test. To practice again.

--- 2_hello_earth/hello.go
+++ 2_hello_earth/hello.go
@@ -5,17 +5,21 @@ import "fmt"
 type language string
 
 func main() {
-	greeting := greet("fr")
+	greeting := greet("cn")
 	fmt.Println(greeting)
 }
 
+var phrasebook = map[language]string{
+	"en": "Hello, world!",
+	"fr": "Bonjour, monde!",
+	"cn": "你好！",
+	"de": "Hallo, welt!",
+}
+
 func greet(l language) string {
-	switch l {
-	case "en":
-		return "Hello, world!"
-	case "fr":
-		return "Bonjour, monde!"
-	default:
-		return ""
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
 	}
+	return greeting
 }
--- 2_hello_earth/main_internal_test.go
+++ 2_hello_earth/main_internal_test.go
@@ -8,35 +8,75 @@ import "testing"
 // 	// Hello, world!
 // }
 
-func TestGreet_English(t *testing.T) {
-	lang := language("en")
-	want := "Hello, world!"
-	got := greet(lang)
+// func TestGreet_English(t *testing.T) {
+// 	lang := language("en")
+// 	want := "Hello, world!"
+// 	got := greet(lang)
 
-	if got != want {
-		// mark test as failed
-		t.Errorf("expected: %q, got %q", want, got)
+// 	if got != want {
+// 		// mark test as failed
+// 		t.Errorf("expected: %q, got %q", want, got)
+// 	}
+// }
+
+// func TestGreet_French(t *testing.T) {
+// 	lang := language("fr")
+// 	want := "Bonjour, monde!"
+// 	got := greet(lang)
+
+// 	if got != want {
+// 		// mark test as failed
+// 		t.Errorf("expected: %q, got %q", want, got)
+// 	}
+// }
+
+// func TestGreet_Akkadian(t *testing.T) {
+// 	lang := language("ak")
+// 	want := ""
+// 	got := greet(lang)
+
+// 	if got != want {
+// 		// mark test as failed
+// 		t.Errorf("expected: %q, got %q", want, got)
+// 	}
+// }
+
+func TestGreet(t *testing.T) {
+	type testCase struct {
+		lang language
+		want string
 	}
-}
 
-func TestGreet_French(t *testing.T) {
-	lang := language("fr")
-	want := "Bonjour, monde!"
-	got := greet(lang)
-
-	if got != want {
-		// mark test as failed
-		t.Errorf("expected: %q, got %q", want, got)
+	var tests = map[language]testCase{
+		"English": {
+			lang: "en",
+			want: "Hello, world!",
+		},
+		"French": {
+			lang: "fr",
+			want: "Bonjour, monde!",
+		},
+		"Chinese": {
+			lang: "cn",
+			want: "你好！",
+		},
+		"German": {
+			lang: "de",
+			want: "Hallo, welt!",
+		},
+		"Undefined": {
+			lang: "",
+			want: `unsupported language: ""`,
+		},
 	}
-}
 
-func TestGreet_Akkadian(t *testing.T) {
-	lang := language("fr")
-	want := "Bonjour, monde!"
-	got := greet(lang)
-
-	if got != want {
-		// mark test as failed
-		t.Errorf("expected: %q, got %q", want, got)
+	for name, tc := range tests {
+		t.Run(string(name), func(t *testing.T) {
+			got := greet(tc.lang)
+			if got != tc.want {
+				t.Errorf("expected %q, got %q", tc.want, got)
+			}
+		})
 	}
+
 }
